69917b00524b2a67258950e3798cc480bb982c46
fdjlss2 <jlschellman@uc.cl>
2017-05-30 19:51:45 -0400

    more stopwords + twitter mask

--- tweets_books_wordcloud.py
+++ tweets_books_wordcloud.py
@@ -3,6 +3,7 @@
 from os import path, listdir
 from os.path import isfile, join
 from wordcloud import WordCloud, STOPWORDS
+from PIL import Image
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
@@ -13,7 +14,8 @@ import matplotlib.pyplot as plt
 tweets_path = 'TwitterRatings/users_goodreads/'
 filenames = [ tweets_path + f for f in listdir(tweets_path) ]
 
-not_words = ['https', 'co', 'goodreads', 'review', 'bit', 'ly', 'id', 'user_status', 'html', 'spref', 'goo', 'gl', 'fb', 'facebook']
+not_words = ['https', 'co', 'goodreads', 'review', 'bit', 'ly', 'id', 'user_status', 'html', 'spref', 'goo', 'gl', 'fb', 'facebook', \
+							'show', 'star', 'instagram', 'blog', 'YouTube', 'Marked', 'page', 'bloglovin', 'amazon', 'dp', 'youtu', 'pinterest', 'pin']
 
 text = ''
 for filename in filenames:
@@ -21,8 +23,10 @@ for filename in filenames:
 	text = text + tweets
 
 stop_words=set( list(STOPWORDS) + not_words)
+twitter_mask = np.array( Image.open("twitter_mask.png") )
 
-wordcloud = WordCloud(max_font_size=40, stopwords=stop_words, max_words=500).generate(text)
+
+wordcloud = WordCloud(background_color="white", max_font_size=40, stopwords=stop_words, max_words=800, mask=twitter_mask).generate(text)
 plt.figure()
 plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
