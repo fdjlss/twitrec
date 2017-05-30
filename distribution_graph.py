11a9818f0a46423a4a70db7f84832c07a9e1d6eb
fdjlss2 <jlschellman@uc.cl>
2017-05-30 13:42:33 -0400

    for checking #tot_tweets (sampled)

--- distribution_graph.py
+++ distribution_graph.py
@@ -10,9 +10,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
+plot_color = "#4285f4"
+plt.rcParams["font.family"] = "Arial"
+
 distribution = []
 
-with open('TwitterRatings/books_distribution.txt', 'r') as f:
+with open('TwitterRatings/books_distribution_sampled.txt', 'r') as f:
 	for line in f:
 		distribution.append( line.strip().split(',') )
 
@@ -23,18 +27,38 @@ distribution.sort( key=lambda x: x[1] )
 
 x = [duple[0] for duple in distribution]
 y = [duple[1] for duple in distribution]
+x2 = [i for i in range(0,len(y))]
+# y.sort()
+# # distribution = pd.DataFrame({ "User ID": x2,
+# # 															"Consumed Books": y })
 
-plt.xlabel("Users IDs")
-plt.ylabel("#Consumed Books")
+# distribution = pd.DataFrame(data=y, index=x2)
 
-distribution = pd.DataFrame({ "User ID": x,
-															"Consumed Books": y })
+# # distribution.sort(columns='Consumed Books', inplace=True, ascending=True)
 
-distribution.sort(columns='Consumed Books', inplace=True, ascending=False)
 
-ax = distribution.plot(y='Consumed Books', kind='bar')
-ax.grid(False)
-# _ = ax.set_xticklabels(distribution['User ID'])
-plt.setp(ax.get_xticklabels(), visible=False)
+# ax = distribution.plot(kind='bar', stacked=False, width=1, title="Distribución de libros por usuarios", legend=False, rot=0, color=plot_color)
+# ax.set_xlabel("")
+# ax.set_ylabel("#Libros consumidos")
 
+# n = 10
+
+# ticks = ax.xaxis.get_ticklocs()
+# ticklabels = [l.get_text() for l in ax.xaxis.get_ticklabels()]
+# ax.xaxis.set_ticks(ticks[::n])
+# ax.xaxis.set_ticklabels(ticklabels[::n])
+
+# plt.setp(ax.get_xticklabels(), visible=True)
+
+# plt.show()
+
+# H_ books / users
+####################
+plt.plot(y, color=plot_color)
+axes = plt.gca()
+axes.set_xlim([min(y), len(y)])
+plt.title("Distribución de libros por usuario (muestreo)")
+plt.ylabel("#Libros consumidos")
+plt.xlabel("")
 plt.show()
+#####################
--- tweets_counter.py
+++ tweets_counter.py
@@ -2,7 +2,7 @@ from os import listdir
 from os.path import isfile, join
 import numpy as np
 import matplotlib
-matplotlib.use('Agg')
+matplotlib.use('TkAgg')
 
 import matplotlib.pyplot as plt
 import json
@@ -12,22 +12,22 @@ import json
 plot_color = "#4285f4"
 plt.rcParams["font.family"] = "Arial"
 ##################################################################
-# path = u"TwitterRatings/users_goodreads/"
-# filenames = [f for f in listdir(path) if isfile(join(path, f))]
+path = u"TwitterRatings/users_goodreads_sampled/"
+filenames = [f for f in listdir(path) if isfile(join(path, f))]
 
-# user_tweet_counter = [0 for i in range(0, len(filenames))]
+user_tweet_counter = [0 for i in range(0, len(filenames))]
 
-# for i in range(0,len(filenames)):
-# 	with open(path+filenames[i], 'r', encoding="ISO-8859-1") as f:
-# 		user_tweet_counter[i] += sum(1 for _ in f)
+for i in range(0,len(filenames)):
+	with open(path+filenames[i], 'r', encoding="ISO-8859-1") as f:
+		user_tweet_counter[i] += sum(1 for _ in f)
 
-# print(sum(user_tweet_counter))
-# print(len(filenames))
-# user_tweet_counter.sort()
+print("Total de tweets:", sum(user_tweet_counter))
+print("Cantidad de usuarios: ", len(filenames))
+user_tweet_counter.sort()
 
-# plt.plot(user_tweet_counter, color=plot_color)
-# axes = plt.gca()
-# axes.set_xlim([0, 4024])
+plt.plot(user_tweet_counter, color=plot_color)
+axes = plt.gca()
+axes.set_xlim([0, 4024])
 # plt.title("Histograma de tuits por usuario (dataset actual)")
 # plt.ylabel("#tweets")
 # plt.xlabel("")
@@ -70,7 +70,7 @@ plt.rcParams["font.family"] = "Arial"
 # Método antiguo
 # ##################################################################
 # ratings = [0, 0, 0, 0, 0, 0]
-# with open("TwitterRatings/ratings.txt", 'r') as f:
+# with open("TwitterRatings/ratings_sampled.txt", 'r') as f:
 # 	for line in f:
 # 		line = line.strip().split(",")
 # 		if line[2] == '0':
@@ -87,10 +87,10 @@ plt.rcParams["font.family"] = "Arial"
 # 			ratings[5] += 1
 
 
-# plt.bar([0, 1, 2, 3, 4, 5], ratings, align='center', color=plot_color)
+# plt.bar([0, 1, 2, 3, 4, 5], ratings, align='center', edgecolor="none", color=plot_color)
 # axes = plt.gca()
 # axes.set_xlim([0.5,5.5])
-# plt.title("Distribución de ratings ")
+# plt.title("Distribución de ratings (muestreo)")
 # plt.ylabel("Cuentas")
 # plt.xlabel("Rating")
 # plt.show()
@@ -101,47 +101,47 @@ plt.rcParams["font.family"] = "Arial"
 
 
 # Método nuevo
-# Conexion con BD
-import sqlite3
 # ##################################################################
-counts = [0, 0, 0, 0, 0, 0]
+# Conexion con BD
+# import sqlite3
+# counts = [0, 0, 0, 0, 0, 0]
 
-sqlite_file = 'db/goodreads.sqlite'
-conn = sqlite3.connect(sqlite_file)
-conn.row_factory = lambda cursor, row: row[0]
-c = conn.cursor()
+# sqlite_file = 'db/goodreads.sqlite'
+# conn = sqlite3.connect(sqlite_file)
+# conn.row_factory = lambda cursor, row: row[0]
+# c = conn.cursor()
 
-ratings = c.execute("SELECT rating FROM user_reviews").fetchall()
-print( "#ratings = {0}".format(len(ratings)) )
+# ratings = c.execute("SELECT rating FROM user_reviews").fetchall()
+# print( "#ratings = {0}".format(len(ratings)) )
 
-with open("TwitterRatings/ratings.txt", 'r') as f:
-	for rating in ratings:
-		if rating == 0:
-			counts[0] += 1
-		if rating == 1:
-			counts[1] += 1
-		if rating == 2:
-			counts[2] += 1
-		if rating == 3:
-			counts[3] += 1
-		if rating == 4:
-			counts[4] += 1
-		if rating == 5:
-			counts[5] += 1
+# with open("TwitterRatings/ratings.txt", 'r') as f:
+# 	for rating in ratings:
+# 		if rating == 0:
+# 			counts[0] += 1
+# 		if rating == 1:
+# 			counts[1] += 1
+# 		if rating == 2:
+# 			counts[2] += 1
+# 		if rating == 3:
+# 			counts[3] += 1
+# 		if rating == 4:
+# 			counts[4] += 1
+# 		if rating == 5:
+# 			counts[5] += 1
 
 
-plt.bar([0, 1, 2, 3, 4, 5], counts, align='center', color=plot_color)
-axes = plt.gca()
-axes.set_xlim([-0.5,5.5])
-plt.title("Distribución de ratings ")
-plt.ylabel("Cuentas")
-plt.xlabel("Rating")
-# plt.savefig('db/h_ratings2.png')
+# plt.bar([0, 1, 2, 3, 4, 5], counts, align='center', color=plot_color)
+# axes = plt.gca()
+# axes.set_xlim([-0.5,5.5])
+# plt.title("Distribución de ratings ")
+# plt.ylabel("Cuentas")
+# plt.xlabel("Rating")
+# # plt.savefig('db/h_ratings2.png')
 
-print( "#ratings (no 0) = {0}".format(sum(counts[1:])) )
-print( "#ratings 1 = {0}".format(counts[1]*100/79889) )
-print( "#ratings 2 = {0}".format(counts[2]*100/79889) )
-print( "#ratings 3 = {0}".format(counts[3]*100/79889) )
-print( "#ratings 4 = {0}".format(counts[4]*100/79889) )
-print( "#ratings 5 = {0}".format(counts[5]*100/79889) )
+# print( "#ratings (no 0) = {0}".format(sum(counts[1:])) )
+# print( "#ratings 1 = {0}".format(counts[1]*100/79889) )
+# print( "#ratings 2 = {0}".format(counts[2]*100/79889) )
+# print( "#ratings 3 = {0}".format(counts[3]*100/79889) )
+# print( "#ratings 4 = {0}".format(counts[4]*100/79889) )
+# print( "#ratings 5 = {0}".format(counts[5]*100/79889) )
 # ##################################################################
