d3a02aea68d941b69ba6860786698ecf3e0d8833
fdjlss2 <jlschellman@uc.cl>
2017-05-30 20:12:09 -0400

    send books_mask

--- tweets_books_wordcloud.py
+++ tweets_books_wordcloud.py
@@ -10,26 +10,51 @@ matplotlib.use('Agg')
 
 import matplotlib.pyplot as plt
 
+# Users
 #################################################################
-tweets_path = 'TwitterRatings/users_goodreads/'
-filenames = [ tweets_path + f for f in listdir(tweets_path) ]
+# tweets_path = 'TwitterRatings/users_goodreads/'
+# filenames = [ tweets_path + f for f in listdir(tweets_path) ]
 
-not_words = ['https', 'co', 'goodreads', 'review', 'bit', 'ly', 'id', 'user_status', 'html', 'spref', 'goo', 'gl', 'fb', 'facebook', \
-							'show', 'star', 'instagram', 'blog', 'YouTube', 'Marked', 'page', 'bloglovin', 'amazon', 'dp', 'youtu', 'pinterest', 'pin']
+# not_words = ['https', 'co', 'goodreads', 'review', 'bit', 'ly', 'id', 'user_status', 'html', 'spref', 'goo', 'gl', 'fb', 'facebook', \
+# 							'show', 'star', 'instagram', 'blog', 'YouTube', 'Marked', 'page', 'bloglovin', 'amazon', 'dp', 'youtu', 'pinterest', 'pin']
+
+# text = ''
+# for filename in filenames:
+# 	tweets = open(filename, 'r', encoding='utf-8', errors='ignore').read()
+# 	text = text + tweets
+
+# stop_words=set( list(STOPWORDS) + not_words)
+# twitter_mask = np.array( Image.open("twitter_mask.png") )
+
+
+# wordcloud = WordCloud(background_color="white", max_font_size=50, stopwords=stop_words, max_words=500, mask=twitter_mask).generate(text)
+# plt.figure()
+# plt.imshow(wordcloud, interpolation="bilinear")
+# plt.axis("off")
+# plt.savefig('tweets_wordcloud.png')
+#################################################################
+
+
+# Books
+#################################################################
+books_path = 'TwitterRatings/items_goodreads/'
+filenames = [ books_path + f for f in listdir(books_path) ]
+
+# not_words = ['https', 'co', 'goodreads', 'review', 'bit', 'ly', 'id', 'user_status', 'html', 'spref', 'goo', 'gl', 'fb', 'facebook', \
+							# 'show', 'star', 'instagram', 'blog', 'YouTube', 'Marked', 'page', 'bloglovin', 'amazon', 'dp', 'youtu', 'pinterest', 'pin']
 
 text = ''
 for filename in filenames:
-	tweets = open(filename, 'r', encoding='utf-8', errors='ignore').read()
-	text = text + tweets
+	info = open(filename, 'r', encoding='utf-8', errors='ignore').read()
+	text = text + info
 
-stop_words=set( list(STOPWORDS) + not_words)
-twitter_mask = np.array( Image.open("twitter_mask.png") )
+stop_words=set( list(STOPWORDS) )
+book_mask = np.array( Image.open("book_mask.png") )
 
 
-wordcloud = WordCloud(background_color="white", max_font_size=50, stopwords=stop_words, max_words=500, mask=twitter_mask).generate(text)
+wordcloud = WordCloud(background_color="white", max_font_size=50, stopwords=stop_words, max_words=500, mask=book_mask).generate(text)
 plt.figure()
 plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
-plt.savefig('tweets_wordcloud.png')
-
+plt.savefig('books_wordcloud.png')
 #################################################################
