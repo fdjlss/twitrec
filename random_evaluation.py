3b7ec280a90aeed7e2c3d027b42fd773e5f6c067
fdjlss2 <jlschellman@uc.cl>
2017-12-04 19:12:18 -0300

    updated random evaluation for new dataset splits

--- random_evaluation.py
+++ random_evaluation.py
@@ -4,12 +4,12 @@ import sqlite3
 import re, json
 import os
 from random import sample
-from jojFunkSvd import mean, stddev, rel_div, DCG, iDCG, nDCG, P_at_N, AP_at_N, consumption, ratingsSampler
+from jojFunkSvd import mean, stdev, MRR, rel_div, DCG, iDCG, nDCG, P_at_N, AP_at_N, R_precision, consumption, user_ranked_recs, opt_value
 from solr_evaluation import remove_consumed
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-
+#-----"PRIVATE" METHODS----------#
 def book_list(db_conn):
 	"""Obtiene una lista de IDs de todos los libros
 	encontrados en la DB (SQLite)
@@ -33,68 +33,44 @@ def book_list(db_conn):
 		book_list.append(book_id)
 
 	return book_list
+#--------------------------------#
 
-def random_eval(db_conn, topN, folds):
-	total_c = consumption(ratings_path='TwitterRatings/funkSVD/ratings.total', rel_thresh=0, with_ratings=True)
+def random_eval(db_conn, N):
+	total_c = consumption(ratings_path=data_path+'eval_all_N'+str(N)+'.data', rel_thresh=0, with_ratings=True)
+	test_c  = consumption(ratings_path=data_path+'test/test_N'+str(N)+'.data', rel_thresh=0, with_ratings=True)
+	train_c = consumption(ratings_path=data_path+'eval_train_N'+str(N)+'.data', rel_thresh=0, with_ratings=False)
 	books   = book_list(db_conn=db_conn)
-	a       = dict((metric, []) for metric in ['nDCG(1)', 'nDCG(2)', 'MAP4', 'MAP3', 'MAP2'])
-	results = dict((n, a) for n in topN)
 
-	ratings_train = []
-	with open('TwitterRatings/funkSVD/ratings.train', 'r') as f:
-		for line in f:
-			ratings_train.append( line.strip() )
+	MRRs          = []
+	nDCGs_bin     = []
+	nDCGs_normal  = []
+	nDCGs_altform = []
+	APs           = []
+	Rprecs        = []
 
-	for _ in range(0,folds):
-		nDCGs_normal  = dict((n, []) for n in topN)
-		nDCGs_altform = dict((n, []) for n in topN)
-		APs_thresh4   = dict((n, []) for n in topN)
-		APs_thresh3   = dict((n, []) for n in topN)
-		APs_thresh2   = dict((n, []) for n in topN)
-		ratingsSampler(rats=ratings_train, fout='TwitterRatings/funkSVD/ratings_temp.train', n=0.8)
-		train_c = consumption(ratings_path='TwitterRatings/funkSVD/ratings_temp.train', rel_thresh=0, with_ratings=False)
-		for userId in train_c:
-			book_recs = sample(books, k=200)
-			book_recs = remove_consumed(user_consumption=train_c[userId], rec_list=book_recs)
+	for userId in train_c:
+		book_recs = sample(books, k=200)
+		book_recs = remove_consumed(user_consumption=train_c[userId], rec_list=book_recs)
+		recs      = user_ranked_recs(user_recs=book_recs, user_consumpt=test_c[userId])
+		mini_recs = dict((k, recs[k]) for k in recs.keys()[:N])
 
-			recs = {}
-			place = 1
-			for itemId in book_recs:
-				if itemId in total_c[userId]:
-					rating = int( total_c[userId][itemId] )
-				else:
-					rating = 0
-				recs[place] = rating
-				place += 1
+		MRRs.append( MRR(recs=recs, rel_thresh=1) )
+		nDCGs_bin.append( nDCG(recs=mini_recs, alt_form=False, rel_thresh=1) )
+		nDCGs_normal.append( nDCG(recs=mini_recs, alt_form=False, rel_thresh=False) )
+		nDCGs_altform.append( nDCG(recs=mini_recs, alt_form=True, rel_thresh=False) )			
+		APs.append( AP_at_N(n=N, recs=recs, rel_thresh=1) )
+		Rprecs.append( R_precision(n_relevants=N, recs=mini_recs) )
 
-			for n in topN: 
-				mini_recs = dict((k, recs[k]) for k in recs.keys()[:n])
-				nDCGs_normal[n].append( nDCG(recs=mini_recs, alt_form=False) )
-				nDCGs_altform[n].append( nDCG(recs=mini_recs, alt_form=True) )			
-				APs_thresh4[n].append( AP_at_N(n=n, recs=recs, rel_thresh=4) )
-				APs_thresh3[n].append( AP_at_N(n=n, recs=recs, rel_thresh=3) )
-				APs_thresh2[n].append( AP_at_N(n=n, recs=recs, rel_thresh=2) )
-
-		for n in topN:
-			results[n]['nDCG(1)'].append( mean(nDCGs_normal[n]) )
-			results[n]['nDCG(2)'].append( mean(nDCGs_altform[n]) ) 
-			results[n]['MAP4'].append( mean(APs_thresh4[n]) ) 
-			results[n]['MAP3'].append( mean(APs_thresh3[n]) ) 
-			results[n]['MAP2'].append( mean(APs_thresh2[n]) ) 
-
-
-	for n in topN:
-		with open('TwitterRatings/random/results.txt', 'a') as file:
-			file.write("\nn={}".format(n))
-			# Orden: nDCG(1) - nDCG(2) - MAP4 - MAP3 - MAP2
-			for metric in results[n]:
-				file.write( ", {0}={1}".format(metric, mean(results[n][metric])) )		
-
+	with open('TwitterRatings/random/results.txt', 'a') as file:
+		file.write( "N=%s, normal nDCG=%s, alternative nDCG=%s, bin nDCG=%s, MAP=%s, MRR=%s, R-precision=%s\n" % \
+				(N, mean(nDCGs_normal), mean(nDCGs_altform), mean(nDCGs_bin), mean(APs), mean(MRRs), mean(Rprecs)) )
 
 def main():
 	sqlite_file = 'db/goodreads.sqlite'
+	data_path = 'TwitterRatings/funkSVD/data/'
 	conn = sqlite3.connect(sqlite_file)
-	random_eval(db_conn=conn, topN=[5, 10, 15, 20, 50], folds=10)
+	for N in [5, 10, 15, 20]:
+		random_eval(db_conn=conn, N=N)
 	conn.close()
 
 if __name__ == '__main__':
