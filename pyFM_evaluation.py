00808c76bb5307857d86e1c1dbf355e86c19f463
fdjlss2 <jlschellman@uc.cl>
2018-01-22 14:33:19 -0300

    REALLY changed default values of f and mi

--- pyFM_evaluation.py
+++ pyFM_evaluation.py
@@ -6,9 +6,8 @@ from solr_evaluation import remove_consumed
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 import numpy as np
-from scipy.sparse import coo_matrix, csr_matrix, csc_matrix, hstack
+from scipy.sparse import coo_matrix, csr_matrix, csc_matrix
 import pandas as pd
-from implicit_evaluation import IdCoder
 from pyfm import pylibfm
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.metrics import mean_squared_error, mean_absolute_error
@@ -27,7 +26,7 @@ def loadData(filename, data_path='TwitterRatings/funkSVD/data/'):
       items.add(itemId)
   return data, np.array(y), items
 
-def FMJob(data_path, params, N, vectorizer):
+def pyFMJob(data_path, params, N, vectorizer):
 	rmses = []
 	logging.info("Evaluando con params: {0}".format(params))
 	for i in range(1, 4+1):
@@ -60,42 +59,42 @@ def pyFM_tuning(data_path, N):
 							'invscale_pow': 0.5, 'optimal_denom': 0.001, 'shuffle': True, 'seed': 28} #cambio del original: f:20, mi:1
 	results  = dict((param, {}) for param in defaults.keys())
 
-	for param in ['f', 'mi', 'bias', 'oneway', 'init_stdev', 'val_size', 'lr_s', 'lr', 'invscale_pow', 'optimal_denom', 'shuffle', 'seed']:
+	for param in ['mi', 'f', 'bias', 'oneway', 'init_stdev', 'val_size', 'lr_s', 'lr', 'invscale_pow', 'optimal_denom', 'shuffle', 'seed']:
 
 		if param=='mi':
 			for i in [1, 5, 10, 20, 50, 100, 150, 200]: 
 				defaults['mi'] = i
-				results['mi'][i] = FMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
+				results['mi'][i] = pyFMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
 			defaults['mi'] = opt_value(results= results['mi'], metric= 'rmse')
 
 		elif param=='f': 
 			for i in range(20, 2020, 20):
 				defaults['f'] = i
-				results['f'][i] = FMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
+				results['f'][i] = pyFMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
 			defaults['f'] = opt_value(results= results['f'], metric= 'rmse')
 
 		elif param=='bias':
 			for i in [True, False]: 
 				defaults['bias'] = i
-				results['bias'][i] = FMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
+				results['bias'][i] = pyFMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
 			defaults['bias'] = opt_value(results= results['bias'], metric= 'rmse')
 
 		elif param=='oneway':
 			for i in [True, False]: 
 				defaults['oneway'] = i
-				results['oneway'][i] = FMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
+				results['oneway'][i] = pyFMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
 			defaults['oneway'] = opt_value(results= results['oneway'], metric= 'rmse')
 
 		elif param=='init_stdev':
 			for i in [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1.0]: 
 				defaults['init_stdev'] = i
-				results['init_stdev'][i] = FMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
+				results['init_stdev'][i] = pyFMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
 			defaults['init_stdev'] = opt_value(results= results['init_stdev'], metric= 'rmse')
 
 		elif param=='val_size':
 			for i in [0.001, 0.01, 0.1, 0.5, 0.8, 0.9]: 
 				defaults['val_size'] = i
-				results['val_size'][i] = FMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
+				results['val_size'][i] = pyFMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
 			defaults['val_size'] = opt_value(results= results['val_size'], metric= 'rmse')
 
 		elif param=='lr_s':
@@ -105,38 +104,38 @@ def pyFM_tuning(data_path, N):
 				if i=='optimal':
 					for j in [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5]:
 						defaults['optimal_denom'] = j
-						results['optimal_denom'][j] = FMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
+						results['optimal_denom'][j] = pyFMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
 					defaults['optimal_denom'] = opt_value(results= results['optimal_denom'], metric= 'rmse')
 					results['lr_s'][i] = results['optimal_denom'][ defaults['optimal_denom'] ]
 
 				elif i=='invscaling':
 					for j in [0.001, 0.05, 0.1, 0.5, 0.8, 1.0]:
 						defaults['invscale_pow'] = j
-						results['invscale_pow'][j] = FMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
+						results['invscale_pow'][j] = pyFMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
 					defaults['invscale_pow'] = opt_value(results= results['invscale_pow'], metric= 'rmse')
 					results['lr_s'][i] = results['invscale_pow'][ defaults['invscale_pow'] ]
 
 				elif i=='constant':
-					results['lr_s'][i] = FMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
+					results['lr_s'][i] = pyFMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
 
 			defaults['lr_s'] = opt_value(results= results['lr_s'], metric= 'rmse')
 
 		elif param=='lr':
 			for i in [0.001, 0.003, 0.005, 0.01, 0.02, 0.03, 0.04, 0.05, 0.07, 0.08, 0.1]: 
 				defaults['lr'] = i
-				results['lr'][i] = FMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
+				results['lr'][i] = pyFMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
 			defaults['lr'] = opt_value(results= results['lr'], metric= 'rmse')
 
 		elif param=='shuffle':
 			for i in [True, False]: 
 				defaults['shuffle'] = i
-				results['shuffle'][i] = FMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
+				results['shuffle'][i] = pyFMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
 			defaults['shuffle'] = opt_value(results= results['shuffle'], metric= 'rmse')
 
 		elif param=='seed':
 			for i in [10, 20, 28, 30, 50]: 
 				defaults['seed'] = i
-				results['seed'][i] = FMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
+				results['seed'][i] = pyFMJob(data_path= data_path, params= defaults, N=N, vectorizer= v)
 			defaults['seed'] = opt_value(results= results['seed'], metric= 'rmse')
 
 
