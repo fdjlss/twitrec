dc1136e316872667fb01080597cc30ca884a6990
fdjlss2 <jlschellman@uc.cl>
2018-04-26 17:11:11 -0300

    training in eng and esp

--- word2vec_evaluation.py
+++ word2vec_evaluation.py
@@ -2,31 +2,37 @@
 
 import gensim.downloader as api
 from gensim.models.word2vec import Word2Vec
-from gensim.parsing.preprocessing import preprocess_string, strip_tags, strip_punctuation, strip_multiple_whitespaces, strip_numeric
+# from gensim.parsing.preprocessing import preprocess_string, strip_tags, strip_punctuation, strip_multiple_whitespaces, strip_numeric
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 import os
-from smart_open import smart_open
-from nltk.corpus import stopwords
-stop_words = set(stopwords.words('spanish') + stopwords.words('english'))
-CUSTOM_FILTERS = [lambda x: x.lower(), strip_tags, strip_punctuation, strip_multiple_whitespaces, strip_numeric]
+# from smart_open import smart_open
+# from nltk.corpus import stopwords
+# stop_words = set(stopwords.words('spanish') + stopwords.words('english'))
+# CUSTOM_FILTERS = [lambda x: x.lower(), strip_tags, strip_punctuation, strip_multiple_whitespaces, strip_numeric]
 
-class SmartCorpus(object):
-  def __init__(self, dirname):
-    self.dirname = dirname
-  def __iter__(self):
-    for fname in os.listdir(self.dirname):
-      for line in smart_open(os.path.join(self.dirname, fname), 'rb'):
-        sentence = preprocess_string(line, CUSTOM_FILTERS)
-        yield [w for w in sentence if w not in stop_words]
+# class SmartCorpus(object):
+#   def __init__(self, dirname):
+#     self.dirname = dirname
+#   def __iter__(self):
+#     for fname in os.listdir(self.dirname):
+#       for line in smart_open(os.path.join(self.dirname, fname), 'rb'):
+#         sentence = preprocess_string(line, CUSTOM_FILTERS)
+#         yield [w for w in sentence if w not in stop_words]
 
-english_corpus = SmartCorpus('/home/jschellman/umbc_corpus/webbase_all/') # UMBC
-model = Word2Vec(sentences=english_corpus, size=300, min_count=5, sg=1, hs=1, negative=5, cbow_mean=1, iter=5)
+# english_corpus = SmartCorpus('/home/jschellman/umbc_corpus/webbase_all/') # UMBC
+# model = Word2Vec(sentences=english_corpus, size=300, min_count=5, sg=1, hs=1, negative=5, cbow_mean=1, iter=5)
+# model.save('./w2v-tmp/w2v-umbc')
+# spanish_corpus = SmartCorpus('/mnt/f90f82f4-c2c7-4e53-b6af-7acc6eb85058/datasets/word2vec_corpus/SBWCE_corpus/spanish_billion_words/') # SBWCE
+# model.build_vocab(spanish_corpus, update=True)
+# model.train(spanish_corpus, total_examples=model.corpus_count, epochs=model.iter)
+# model.save('./w2v-tmp/w2v-bilingual')
 
-model.save('./w2v-tmp/w2v-umbc')
+from gensim.models import KeyedVectors
 
-spanish_corpus = SmartCorpus('/mnt/f90f82f4-c2c7-4e53-b6af-7acc6eb85058/datasets/word2vec_corpus/SBWCE_corpus/spanish_billion_words/') # SBWCE
-
-model.build_vocab(spanish_corpus, update=True)
-model.train(spanish_corpus, total_examples=model.corpus_count, epochs=model.iter)
-model.save('./w2v-tmp/w2v-bilingual')
+cantidad = 100000
+model_esp = KeyedVectors.load_word2vec_format('/home/jschellman/fasttext-sbwc.3.6.e20.vec')
+model_esp.save('./w2v-tmp/model-esp.w2v')
+del model_esp
+model_eng = api.load('word2vec-google-news-300')
+model_eng.save('./w2v-tmp/model-eng.w2v')
