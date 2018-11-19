2c82dc08b3785dbd35d5306382acb94dc5308591
fdjlss <jlschellman@uc.cl>
2018-11-19 16:40:32 -0300

    CLEANED RECS: hybrid

--- hybrid_evaluation.py
+++ hybrid_evaluation.py
@@ -344,13 +344,21 @@ def main():
 	# params de cleaned: cambiado params de solr e implicit (no pyFM: tuning con metrica de rating)
 
 	for lib in ['pyFM', 'implicit']:
-		opt_params_cbcf, opt_params_cfcb = hybrid_tuning(data_path=data_path_context, cf_lib=lib, solr=solr, params_cb=params_cb, params_cf=params_imp, N=20) #"pyFM"->data_path_context, "implicit"->data_path
+		
+		if lib == 'pyFM':
+			params_cf = params_pyfm
+			data = data_path_context
+		elif lib == 'implicit'
+			params_cf = params_imp
+			data = data_path
+
+		opt_params_cbcf, opt_params_cfcb = hybrid_tuning(data_path=data, cf_lib=lib, solr=solr, params_cb=params_cb, params_cf=params_cf, N=20) #"pyFM"->data_path_context, "implicit"->data_path
 		
 		if opt_params_cbcf == opt_params_cfcb:
-			hybrid_protocol_evaluation(data_path=data_path, data_path_context=data_path_context, cf_lib=lib, solr=solr, params_cb=params_cb, params_cf=params_imp, params_hy=opt_params_cbcf, N=20)
+			hybrid_protocol_evaluation(data_path=data_path, data_path_context=data_path_context, cf_lib=lib, solr=solr, params_cb=params_cb, params_cf=params_cf, params_hy=opt_params_cbcf, N=20)
 		else:
-			hybrid_protocol_evaluation(data_path=data_path, data_path_context=data_path_context, cf_lib=lib, solr=solr, params_cb=params_cb, params_cf=params_imp, params_hy=opt_params_cbcf, N=20)
-			hybrid_protocol_evaluation(data_path=data_path, data_path_context=data_path_context, cf_lib=lib, solr=solr, params_cb=params_cb, params_cf=params_imp, params_hy=opt_params_cfcb, N=20)
+			hybrid_protocol_evaluation(data_path=data_path, data_path_context=data_path_context, cf_lib=lib, solr=solr, params_cb=params_cb, params_cf=params_cf, params_hy=opt_params_cbcf, N=20)
+			hybrid_protocol_evaluation(data_path=data_path, data_path_context=data_path_context, cf_lib=lib, solr=solr, params_cb=params_cb, params_cf=params_cf, params_hy=opt_params_cfcb, N=20)
 
 
 
